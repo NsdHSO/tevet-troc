912117df64fd52dc0edf28f68cc2755cc1e4628f
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2025-01-08 03:24:00 +0200

    feat: logout api

--- src/entities/auth/infrastructure/http/routes/v1/post-logout.ts
+++ src/entities/auth/infrastructure/http/routes/v1/post-logout.ts
@@ -1,5 +1,15 @@
 import { FastifyInstance } from 'fastify';
+import { Header } from '../../schema';
 
-export default function logout(app : FastifyInstance) {
-    app.post('/logout', async (req, reply) => {})
+export default function logout(app: FastifyInstance) {
+    app.post('/logout', {
+        onRequest: [app.authenticate],
+        schema: {
+            headers: Header
+        }
+    }, async (req, reply) => {
+        req.revokeToken();
+        reply.code(204);
+
+    });
 }
