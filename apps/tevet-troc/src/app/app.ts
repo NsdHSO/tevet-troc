f86b6d3f339cea86adfb4cef6444bf2a7f13fd38
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2025-02-07 08:03:28 +0200

    feat: added entityt model generator

---
+++ apps/tevet-troc/src/app/app.ts
@@ -0,0 +1,25 @@
+import { FastifyInstance } from 'fastify';
+/* eslint-disable-next-line */
+export interface AppOptions {}
+
+export async function app(fastify: FastifyInstance, opts: AppOptions) {
+  fastify.register(import('@fastify/sensible'));
+  await fastify.register(import('@fastify/swagger'),{
+    swagger: {
+      info: {
+        title: 'API Documentation',
+        description: 'This is the API documentation for our service.',
+        version: '1.0.0'
+      },
+      tags: [
+        { name: 'auth', description: 'Authentication-related routes' } // Define the 'auth' parent tag
+      ],
+    },
+  });
+  await fastify.register(import('@fastify/swagger-ui'), {
+    routePrefix: '/documentation'
+  });
+  fastify.ready(() => {
+    console.log(fastify.printRoutes());
+  });
+}
