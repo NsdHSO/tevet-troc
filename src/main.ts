ecb32446fb9924099bb24a145e0aa19ff28fe51e
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2025-03-15 23:11:47 +0200

    chore:🌻 remove trailing slash

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const allowlist = [
+  'http://localhost:4200',
+  'https://tevet-troc-client.vercel.app',
+];
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -13,6 +30,9 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('hospital')
     .build();
+  app.enableCors({
+    origin: corsOptionsDelegate,
+  });
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
